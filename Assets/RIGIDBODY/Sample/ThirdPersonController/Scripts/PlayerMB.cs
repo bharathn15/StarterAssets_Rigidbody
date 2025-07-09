e24fa0bcfdc81cdae674381fa40e16968da3db67
Bharath <bharath.bglr.15@gmail.com>
2025-07-09 22:30:51 +0530

    Reverted environment colliders include layers and exclude layers to None

--- Assets/RIGIDBODY/Sample/ThirdPersonController/Scripts/PlayerMB.cs
+++ Assets/RIGIDBODY/Sample/ThirdPersonController/Scripts/PlayerMB.cs
@@ -25,41 +25,37 @@ public class PlayerMB : CharacterMB
         {
             m_MoveSpeed = 0;
         }
-        mouseX = CharacterInputData.MouseMove().x;
     }
 
-    float mouseX;
-    float targetRotation;
-    [Tooltip("How fast the character turns to face movement direction")]
-    [Range(0.0f, 1f)]
-    public float RotationSmoothTime = 0.12f;
-    [Range(0.0f, 1f)]
-    public float rotationThreshold = 0.05f;
+    [Tooltip("How fast the character turns to face mouse direction")]
+    [Range(0.0f, 5f)]
+    public float m_RotationSmoothTime = 0.12f;
+    [Range(0.0f, 5f)]
+    public float m_RotationThreshold = 0.05f;
     private float rotationVelocity;
     private float lastTargetRotation;
+    private float currentRotation;
     private void FixedUpdate()
     {
-        if (CharacterInputData.MouseMove().sqrMagnitude > rotationThreshold)
+        // ROTATION
+        if (CharacterInputData.MouseMove().sqrMagnitude > m_RotationThreshold)
         {
+            // Last rotation.
             lastTargetRotation = Mathf.Atan2(CharacterInputData.MouseMove().x, CharacterInputData.MouseMove().y) * Mathf.Rad2Deg +
                                  Camera.main.transform.eulerAngles.y;
+
+            currentRotation = Mathf.LerpAngle(transform.eulerAngles.y, lastTargetRotation, Time.fixedDeltaTime * m_RotateSpeed);
+
+            base.m_Rigidbody.transform.rotation = Quaternion.Euler(0.0f, currentRotation, 0.0f);
         }
-        // Smoothly rotate towards the last valid target rotation
-        float rotation = Mathf.SmoothDampAngle(
-            transform.eulerAngles.y,
-            lastTargetRotation,
-            ref rotationVelocity,
-            RotationSmoothTime
-        );
 
-        // Apply rotation
-        base.m_Rigidbody.transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+        // MOVEMENT
+        if (CharacterInputData.Move() != Vector2.zero && m_GroundCheck)
+        {
+            Vector3 direction = (transform.forward * CharacterInputData.Move().y + transform.right * CharacterInputData.Move().x);
+            var finalPos = transform.position + (direction.normalized * Time.fixedDeltaTime * m_MoveSpeed);
+            base.m_Rigidbody.Move(finalPos, Quaternion.Euler(0.0f, currentRotation, 0.0f));
+        }
 
-        // Movement.
-        if (CharacterInputData.Move() == Vector2.zero && m_GroundCheck == false)
-            return;
-        Vector3 direction = (transform.forward * CharacterInputData.Move().y + transform.right * CharacterInputData.Move().x);
-        var finalPos = transform.position + (direction.normalized * Time.fixedDeltaTime * m_MoveSpeed);
-        base.m_Rigidbody.Move(finalPos, Quaternion.identity);
     }
 }
